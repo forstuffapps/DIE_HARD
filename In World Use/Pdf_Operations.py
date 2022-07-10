45e0e5afff1fa3e1db08a22e1eaeaa37c27d01e3
forstuffapps <88136654+forstuffapps@users.noreply.github.com>
2022-07-10 11:47:19 +0530

    Create Pdf_Operations.py

---
+++ In World Use/Pdf_Operations.py
@@ -0,0 +1,24 @@
+from glob import glob
+
+from pikepdf import Pdf
+
+
+def merge_pdfs(n, name):
+    pdf = Pdf.new()
+
+    n = 2  # number of files
+
+    for i in range(1, n + 1):  # you can change this to browse directories recursively
+
+        with Pdf.open('Folder/f' + str(i) + '.pdf') as src: # It is the location of the pdf files Note: all file formats should be in f* series
+            pdf.pages.extend(src.pages)
+
+    pdf.save('Folder/' + name + '.pdf')
+    pdf.close()
+
+
+merge_pdfs(2, 'merge')
+
+
+
+
