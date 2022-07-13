dcdbe5a7cfe552aae40cbe75b78f14620b23119f
forstuffapps <88136654+forstuffapps@users.noreply.github.com>
2022-07-13 22:59:51 +0530

    Update Recipes_1.py

--- Fooods/Recipes_1.py
+++ Fooods/Recipes_1.py
@@ -1,21 +1,72 @@
 
 
-
-                                                                        """
-
-Recipe_1  
-One side Spiced Omelette
-
-
-                                                                        """
-
   
-
-Recipe_2
+                                                                        """
+Recipe_1
 Tomato Liquefy
+                                                                        """
 
 Ingredients : 
 
 Process
 
-Core
+Mix
+
+
+
+
+                                                                        """
+Recipe_2
+Onion Tomato Mix
+                                                                        """
+Process_1 :
+    Take a bowl
+    put chooped onions
+    put chopped tomatos
+    add chilli powder
+    add salt
+    add any spices if you wish(corriander, pepper, Turmeric...)
+    Mix it well
+    Mix_1 is ready
+    Take the pan
+    put oil or butters (Choice)
+    Now add basic ingredients (Allam_Ellipaya_paste, Avalu, Jilkara, Chilly flakes...)
+    Now add the Mix_1
+    Saute so well (looks like a red chutney)
+    
+    pass
+
+
+Process_2 :
+    Take the pan
+    put oil or butters (Choice)
+    Now add basic ingredients (Allam_Ellipaya_paste, Avalu, Jilkara, Chilly flakes...)
+    put chooped onions
+    add chilli powder
+    add salt
+    Saute well untill onions turn golden yellow (a little brown too)
+    put chopped tomatos
+    add chilli powder
+    add salt
+    Saute so well (looks like a red chutney)
+    Mix is ready
+    
+    pass
+
+Both processes are for same Mix but different style of cooking
+
+Prepare :
+    cut onions into small (very small square shape)
+    cut tomatoes (very small square shape)
+    
+    pass
+
+Making :
+    beat eggs and make a egg curry
+    add rice and make a tomato rice
+    
+    pass
+
+
+
+
